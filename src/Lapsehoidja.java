d5c240cd07a173c3a27bae1665f65175e6bb65e7
triinkraavi <79664056+triinkraavi@users.noreply.github.com>
2021-03-08 22:42:24 +0200

    Uuendatud kood

--- src/Lapsehoidja.java
+++ src/Lapsehoidja.java
@@ -1,2 +1,22 @@
 public class Lapsehoidja {
+    private int firmasiseneNr;
+    private String lapsehoidjaNimi;
+
+    public Lapsehoidja(int firmasiseneNr, String lapsehoidjaNimi) {
+        this.firmasiseneNr = firmasiseneNr;
+        this.lapsehoidjaNimi = lapsehoidjaNimi;
+    }
+
+    public int getFirmasiseneNr() {
+        return firmasiseneNr;
+    }
+
+    public String getLapsehoidjaNimi() {
+        return lapsehoidjaNimi;
+    }
+
+    @Override
+    public String toString() {
+        return firmasiseneNr + " " + lapsehoidjaNimi;
+    }
 }
--- src/Lastehoid.java
+++ src/Lastehoid.java
@@ -1,2 +1,30 @@
+import java.util.Collections;
+import java.util.Scanner;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Lastehoid {
-}
+
+    public static void main(String[] args) throws Exception {
+        File fail = new File("lapsehoidjad.txt");
+
+        try (Scanner sc = new Scanner(fail, "UTF-8")){
+            ArrayList<Lapsehoidja> list1 = new ArrayList<>();
+            while (sc.hasNextLine()) {
+                String rida = sc.nextLine();
+                String[] tükid = rida.split(" ");
+                Lapsehoidja lapsehoidja1 = new Lapsehoidja(Integer.parseInt(tükid[0]), tükid[1]);
+                list1.add(lapsehoidja1);
+                }
+            Lapsehoidja jana = new Lapsehoidja(35,"Jana");
+            list1.add(jana);
+
+
+            }
+
+
+
+        }
+    }
+
