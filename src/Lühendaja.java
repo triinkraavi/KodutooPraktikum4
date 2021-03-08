e8c1375ba0bef6a669284681a40510df379ab609
triinkraavi <79664056+triinkraavi@users.noreply.github.com>
2021-03-08 20:37:44 +0200

    Uuendatud kood

--- src/Lühendaja.java
+++ src/Lühendaja.java
@@ -1,2 +1,35 @@
+import java.util.Scanner;
+import java.io.File;
+
 public class Lühendaja {
-}
+
+
+    public static void main(String[] args) throws Exception {
+        File fail = new File("nimed.txt");
+
+        try (Scanner sc = new Scanner(fail, "UTF-8")){
+            while (sc.hasNextLine()) {
+                String rida = sc.nextLine();
+                String[] tükid = rida.split(" ");
+                StringBuilder str = new StringBuilder();
+                str.append(tükid[tükid.length-1] + " ");
+                for (int i = 0; i < tükid.length-1; i++) {
+                    if(tükid[i].contains("-")){
+                        String[] sidekriipsuga = tükid[i].split("-");
+                        str.append(sidekriipsuga[0].charAt(0) + "-" + sidekriipsuga[1].charAt(0) + ".");
+
+                        }
+                    else {
+                        str.append(tükid[i].charAt(0) + ". ");
+                    }
+
+                }
+                System.out.println(str);
+
+                }
+
+            }
+        }
+
+    }
+
--- src/SõnadeAnalüüs.java
+++ src/SõnadeAnalüüs.java
@@ -3,7 +3,7 @@ public class SõnadeAnalüüs {
         String lause = "Kui Arno isaga koolimajja jõudis, olid tunnid juba alanud.";
         String uuslause = lause.replace(",", "");
         String uuslause2 = uuslause.replace(".", "");
-        System.out.println("Algne lause: " + lause);
+        System.out.println("Algne lause oli: " + lause);
         String[] tükid = uuslause2.split(" ");
         for (int i = 0; i < tükid.length; i++) {
             System.out.println("Sõna '" + tükid[i] + "' pikkus: " + tükid[i].length());
